8378973111546ea4e460e44accfceb9a1f02fbc0
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-01-13 00:09:22 +0100

    cleanup

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,31 +1,13 @@
 package controllers;
 
-import java.util.List;
-
-import models.Person;
-import play.*;
-import play.data.Form;
-import play.db.ebean.Model;
-import play.mvc.*;
-import views.html.*;
-
-import static play.libs.Json.toJson;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Hello World"));
-    }
-    
-    public static Result addPerson(){
-    	Person person = Form.form(Person.class).bindFromRequest().get();
-    	person.save();
-    	return redirect(routes.Application.index());
-    }
-    
-    public static Result getPersons(){
-    	List<Person> persons = new Model.Finder(String.class, Person.class).all();
-    	return ok(toJson(persons));
-    }
+    }    
 
 }
