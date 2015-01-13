8d5f92cbb1634707d09fe994d4ace8795a02bb69
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-01-13 20:12:12 +0100

    Multi language support added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,22 @@
 package controllers;
 
+import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Results;
 import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Super Trumpf Tyrol"));
-    }    
+        
+        String msg = Messages.get("files.test");
+        return ok(index.render(msg));
+    }
+
+    public static Result langs() {
+        return ok(request().acceptLanguages().toString());
+        
+    }
 
 }
