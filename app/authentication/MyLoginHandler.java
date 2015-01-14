a30abddf985ac6adcf38697a427f0ed4114885d7
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-01-14 16:51:35 +0100

    Naming bugs removed

--- app/authentication/MyLoginHandler.java
+++ app/authentication/MyLoginHandler.java
@@ -1,28 +1,22 @@
 package authentication;
 
 import contracts.login.LoginHandler;
-import contracts.model.User;
-import contrats.data.DataProvider;
 
-import javax.activation.DataHandler;
+import contracts.data.DataProvider;
+
 import java.util.UUID;
 
-/**
- * Created by adanek on 14/01/15.
- */
 public class MyLoginHandler implements LoginHandler {
 
     private DataProvider dp;
     
     public MyLoginHandler(){
-        
         this.dp=null;
     }
     @Override
     public UUID authenticate(String email, String password) {
-        
-        //User u = this.dp.getUser(email, password);
-        
+
+        //User u = this.dp.getUser(email, password);        
         return UUID.randomUUID();
     }
 }
--- app/contracts/data/DataProvider.java
+++ app/contracts/data/DataProvider.java
@@ -1,10 +1,7 @@
-package contrats.data;
+package contracts.data;
 
 import contracts.model.User;
 
-/**
- * Created by adanek on 14/01/15.
- */
 public interface DataProvider {
     
     User getUser(String email, String hash);
