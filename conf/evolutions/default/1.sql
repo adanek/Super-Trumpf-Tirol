94f2985466126e858d711fc1fbb70cb854e082d3
DaniWi <daniel.witsch@student.uibk.ac.at>
2015-01-20 14:59:00 +0100

    khjklj

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -30,7 +30,7 @@ create table user (
   name                      varchar(255),
   email                     varchar(255),
   password                  varchar(255),
-  constraint pk_user primary key (id))
+  constraint pk_user primary key (id)).
 ;
 
 
