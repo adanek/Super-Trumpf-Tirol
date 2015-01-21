cd9975b8bc6dff012268df55e9f8dfd2f2962198
Andreas Danek <a.danek@live.com>
2015-01-21 14:28:48 +0100

    routing fixed for game

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -11,79 +11,90 @@ import contracts.game.GameStatus;
 
 public class GameController extends Controller {
 
-	// GET /game/selectMode
-	@Security.Authenticated(MyAuthenticator.class)
-	public static Result selectMode() {
+    // GET /game
+    @Security.Authenticated(MyAuthenticator.class)
+    public static Result index() {
+        String pid = session().get("pid");
+        String gid= session().get("gid");
+        
+        GameHandler gh = ServiceLocator.getGameHandler();
 
-		// get pid from request
-		String pid = request().username();
+        return ok(views.html.game.main.render(gh.getCard(gid, pid)));
+    }
 
-		return ok(views.html.game.mode.render());
-	}
+    // GET /game/selectMode
+    @Security.Authenticated(MyAuthenticator.class)
+    public static Result selectMode() {
+
+        // get pid from request
+        String pid = request().username();
+
+        return ok(views.html.game.mode.render());
+    }
 
     // POST /game/create
     @Security.Authenticated(MyAuthenticator.class)
     public static Result createGame() {
 
-	GameHandler gh = ServiceLocator.getGameHandler();
-	String pid = session().get("pid");
-	String gid;
+        GameHandler gh = ServiceLocator.getGameHandler();
+        String pid = session().get("pid");
+        String gid;
 
-	try {
-	    gid = gh.createNewGame(pid);
-	    session("gid", gid);
-	} catch (Exception ex) {
-	    return badRequest();
-	}
+        try {
+            gid = gh.createNewGame(pid);
+            session("gid", gid);
+        } catch (Exception ex) {
+            return badRequest();
+        }
 
-	return ok(views.html.game.main.render(gh.getCard(gid, pid)));
+        return redirect(controllers.routes.GameController.index());
     }
 
     // POST /game/play
     @Security.Authenticated(MyAuthenticator.class)
     public static Result playCard() {
 
-	try {
+        try {
 
-	    String pid = session().get("pid");
-	    String gid = session().get("gid");
-	    int cid = Integer.parseInt(request().body().asFormUrlEncoded().get("cid")[0]);
+            String pid = session().get("pid");
+            String gid = session().get("gid");
+            int cid = Integer.parseInt(request().body().asFormUrlEncoded().get("cid")[0]);
 
-	    ServiceLocator.getGameHandler().makeMove(gid, pid, cid);
-	} catch (Exception ex) {
-	    return badRequest(ex.getMessage());
-	}
+            ServiceLocator.getGameHandler().makeMove(gid, pid, cid);
+        } catch (Exception ex) {
+            return badRequest(ex.getMessage());
+        }
 
-	return ok();
+        return ok();
     }
 
     // GET /game/status
     @Security.Authenticated(MyAuthenticator.class)
     public static Result getStatus() {
 
-		GameStatus state;
+        GameStatus state;
 
-		try {
-			String pid = session().get("pid");
-			String gid = session().get("gid");
-			state = ServiceLocator.getGameHandler().getGameStatus(gid, pid);
-	} catch (Exception ex) {
+        try {
+            String pid = session().get("pid");
+            String gid = session().get("gid");
+            state = ServiceLocator.getGameHandler().getGameStatus(gid, pid);
+        } catch (Exception ex) {
 
-	    return badRequest(ex.getMessage());
-	}
+            return badRequest(ex.getMessage());
+        }
 
-	return ok(Json.toJson(state));
+        return ok(Json.toJson(state));
     }
 
     // POST /game/commit
     public static Result commitRound() {
 
-	return TODO;
+        return TODO;
     }
 
     // POST /game/abord
     public static Result abortGame() {
 
-	return TODO;
+        return TODO;
     }
 }
